7a51790249db45e48528e691cc6e9920b9478e5f
Charles Wong <nok.wong@gmail.com>
2018-08-07 01:34:18 +0000

    add ingredients exercise

---
+++ ingredients.js
@@ -0,0 +1,27 @@
+var ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+/*
+var i = 0;
+while(i < ingredients.length){
+    console.log(ingredients[i]);
+    i++;
+}
+console.log(ingredients.length);
+
+
+for (i = 0; i < ingredients.length; i++){
+    console.log(ingredients[i]);
+}
+
+
+for (i = ingredients.length-1; i >=0; i--){
+    console.log(ingredients[i]);
+}
+
+*/
+
+i = ingredients.length-1;
+while (i >=0){
+    console.log(ingredients[i]);
+    i--;
+}
